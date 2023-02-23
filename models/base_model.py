9644a69bb4b8697ce5cea523ae24578016977a08
Stephen30o0 <o.olurinola@alustudent.com>
2023-02-24 01:57:27 +0200

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -2,30 +2,36 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-
+import models
+from sqlalchemy import Column, String, DateTime
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """The BaseModel class for all storage objects"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        """Initialize a new BaseModel object"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            if 'id' not in kwargs.keys():
+                setattr(self, 'id', str(uuid4()))
+            time = datetime.now()
+            setattr(self, 'created_at', time)
+            setattr(self, 'updated_at', time)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            setattr(self, 'id', str(uuid4()))
+            time = datetime.now()
+            setattr(self, 'created_at', time)
+            setattr(self, 'updated_at', time)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """Return the string representation of a BaseModel object"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
