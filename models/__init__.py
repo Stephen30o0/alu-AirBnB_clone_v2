639368bcdb26b14c8f5e1c1a6dad4b20b88da439
Stephen30o0 <o.olurinola@alustudent.com>
2023-02-24 03:19:42 +0200

    changes

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 """This module defines a unique FileStorage instance"""
 import os
+from models.state import State
+from models.city import City
+from models.user import User
+from models.review import Review
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.place import Place
+
+
+classes = {"User": User, "BaseModel": BaseModel,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
 
 if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
--- models/city.py
+++ models/city.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import os
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
 
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 class City(BaseModel, Base):
     __tablename__ = 'cities'
-
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete-orphan")
+    else:
+        name = ""
+        state_id = ""
