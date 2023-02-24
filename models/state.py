df7744c48cf61c18ed0a9efe21e7ab4c4d9dd214
Stephen30o0 <o.olurinola@alustudent.com>
2023-02-24 02:07:11 +0200

    update to models

--- models/state.py
+++ models/state.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
+class State(BaseModel, Base):
+    """The State class, which inherits from BaseModel and Base"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
 
-class State(BaseModel):
-    """ State class """
-    name = ""
