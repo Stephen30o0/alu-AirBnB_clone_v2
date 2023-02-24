d48ea47a7242ad41e2967d7acc1d4546a5bae280
Stephen30o0 <o.olurinola@alustudent.com>
2023-02-24 03:15:18 +0200

    changes

--- models/state.py
+++ models/state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from models.city import City
 
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 class State(BaseModel, Base):
     __tablename__ = 'states'
@@ -14,6 +16,9 @@ class State(BaseModel, Base):
     if storage_type == 'db':
         cities = relationship('City', cascade='all, delete-orphan', backref='state')
     else:
+        name = ""
+
+    if storage_type != 'db':
         @property
         def cities(self):
             from models import storage
