373c833ce483cba01da9500d2e48fd5c2aa8efb9
Stephen30o0 <o.olurinola@alustudent.com>
2023-03-09 13:03:43 +0200

    cahnges

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from fabric.api import *
+from datetime import datetime
+import os
+
+env.hosts = ['localhost']
+
+
+def do_pack():
+    """Creates a compressed archive of the web_static folder"""
+    try:
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
+        return None
+
