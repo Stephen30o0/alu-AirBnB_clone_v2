e40ffe44233926147404160cbc1af704430cd445
Stephen30o0 <o.olurinola@alustudent.com>
2023-03-09 20:37:25 +0200

    pep8

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -27,17 +27,20 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         filename = archive_path.split("/")[-1].split(".")[0]
         run("mkdir -p /data/web_static/releases/{}/".format(filename))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/\releases/{}/".format(filename, filename))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}\
+            /".format(filename, filename))
         run("rm /tmp/{}.tgz".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename, filename))
+        run("mv /data/web_static/releases/{}/web_static/*\
+             /data/web_static/releases/{}/".format(filename, filename))
         run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
+        run("ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current".format(filename))
         return True
 
     except:
         return False
-    
+
 
 def deploy():
     """Create and distribute an archive to the web servers"""
@@ -45,3 +48,4 @@ def deploy():
     if not archive_path:
         return False
     return do_deploy(archive_path)
+    
