77f4aa1cbdf6a81d80282b682c73f6f90a68ae30
Stephen30o0 <o.olurinola@alustudent.com>
2023-03-09 20:30:34 +0200

    pep 8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,12 +27,15 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         filename = archive_path.split("/")[-1].split(".")[0]
         run("mkdir -p /data/web_static/releases/{}/".format(filename))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(filename, filename))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"\
+            .format(filename, filename))
         run("rm /tmp/{}.tgz".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename, filename))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(filename, filename))
         run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
+        run("ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current".format(filename))
         return True
 
     except:
