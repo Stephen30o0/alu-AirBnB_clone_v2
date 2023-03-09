aa4823c7af871fb9f8bfd01de37a4edf2d45495d
Stephen30o0 <o.olurinola@alustudent.com>
2023-03-09 20:19:56 +0200

    NO 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+from fabric.api import *
+from os.path import exists
+from datetime import datetime
+
+env.hosts = ['54.242.46.167', '54.234.143.129']
+
+
+def do_pack():
+    try:
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to the web servers"""
+    if not exists(archive_path):
+        return False
+
+    try:
+        put(archive_path, "/tmp/")
+        filename = archive_path.split("/")[-1].split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(filename, filename))
+        run("rm /tmp/{}.tgz".format(filename))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
+        return True
+
+    except:
+        return False
+    
+
+def deploy():
+    """Create and distribute an archive to the web servers"""
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
