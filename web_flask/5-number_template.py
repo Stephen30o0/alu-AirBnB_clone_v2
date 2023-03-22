6855732819386db0f31b5585351aa858d0f20ea0
Stephen30o0 <o.olurinola@alustudent.com>
2023-03-22 21:02:43 +0200

    changeschanges

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,9 +33,9 @@ def number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>', methods=['GET'], strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', number=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
