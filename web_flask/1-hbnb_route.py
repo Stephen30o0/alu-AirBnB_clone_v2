41aae04d196f7d505212f9fb66294f351c97da0d
Stephen30o0 <o.olurinola@alustudent.com>
2023-03-22 20:39:33 +0200

    pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,9 +6,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', methods=['GET'], strict_slashes=False)
 def hbnb():
